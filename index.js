236772265659e9c97b2b269f823e8dd78913b804
minami-nakajima <minami.nakajima@linecorp.com>
2020-08-07 15:51:32 +0900

    init liff workshop sample

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
+const https = require('https');
 const app = express();
 const port = process.env.PORT || 5000;
 const myLiffId = process.env.MY_LIFF_ID;
+const redirectUri = process.env.REDIRECT_URI;
 
 app.use(express.static('public'));
 
 app.get('/send-id', function(req, res) {
-    res.json({id: myLiffId});
+    res.json({id: myLiffId, redirectUri });
 });
 
-app.listen(port, () => console.log(`app listening on port ${port}!`));
+if (process.env.NODE_ENV === 'development') {
+    const devCert = fs.readFileSync(
+        path.resolve(__dirname, 'cert/localhost.pem')
+    );
+    const devKey = fs.readFileSync(
+        path.resolve(__dirname, 'cert/localhost-key.pem')
+    );
+    const server = https.createServer({
+        key: devKey,
+        cert: devCert
+    }, app);
+    server.listen(8000, function() {
+        console.log(`https listening on port 8000!`);
+    });
+}
+app.listen(port, () => console.log(`http listening on port ${port}!`));
