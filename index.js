2b6d75d72d11d71923ece1aaa691a8547bd8df29
Chien Cheng Yi <cokela@gmail.com>
2020-10-12 13:37:24 +0800

    init repo

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
+const https = require('https');
 const app = express();
 const port = process.env.PORT || 5000;
 const myLiffId = process.env.MY_LIFF_ID;
+const devCert = fs.readFileSync(
+    path.resolve(__dirname, 'cert/localhost.pem')
+);
+const devKey = fs.readFileSync(
+    path.resolve(__dirname, 'cert/localhost-key.pem')
+);
 
 app.use(express.static('public'));
 
@@ -9,4 +18,13 @@ app.get('/send-id', function(req, res) {
     res.json({id: myLiffId});
 });
 
+if (process.env.NODE_ENV === 'development') {
+    const server = https.createServer({
+        key: devKey,
+        cert: devCert
+    }, app);
+    server.listen(8000, function() {
+        console.log(`app listening on port 8000`);
+    });
+}
 app.listen(port, () => console.log(`app listening on port ${port}!`));
