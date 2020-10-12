4bf3d07bf25271b65658e9e508fb8a240712f38d
Chien Cheng Yi <cokela@gmail.com>
2020-10-12 15:27:11 +0800

    share fixed flex message

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -101,10 +101,75 @@ function displayIsInClientInfo() {
 */
 function registerButtonHandlers() {
     document.getElementById('shareTargetPicker').addEventListener('click', function () {
+        const memeContent = {
+            "type": "bubble",
+            "body": {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "image",
+                  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/flexsnapshot/clip/clip3.jpg",
+                  "size": "full",
+                  "aspectMode": "cover",
+                  "aspectRatio": "1:1",
+                  "gravity": "center"
+                },
+                {
+                  "type": "box",
+                  "layout": "vertical",
+                  "contents": [],
+                  "position": "absolute",
+                  "background": {
+                    "type": "linearGradient",
+                    "angle": "0deg",
+                    "endColor": "#00000000",
+                    "startColor": "#00000099"
+                  },
+                  "width": "100%",
+                  "height": "40%",
+                  "offsetBottom": "0px",
+                  "offsetStart": "0px",
+                  "offsetEnd": "0px"
+                },
+                {
+                  "type": "box",
+                  "layout": "horizontal",
+                  "contents": [
+                    {
+                      "type": "box",
+                      "layout": "vertical",
+                      "contents": [
+                        {
+                          "type": "box",
+                          "layout": "horizontal",
+                          "contents": [
+                            {
+                              "type": "text",
+                              "text": "Brown Grand Hotel",
+                              "size": "xl",
+                              "color": "#ffffff"
+                            }
+                          ]
+                        }
+                      ],
+                      "spacing": "xs"
+                    }
+                  ],
+                  "position": "absolute",
+                  "offsetBottom": "0px",
+                  "offsetStart": "0px",
+                  "offsetEnd": "0px",
+                  "paddingAll": "20px"
+                }
+              ],
+              "paddingAll": "0px"
+            }
+        };
         if (liff.isApiAvailable('shareTargetPicker')) {
             liff.shareTargetPicker([{
-                'type': 'text',
-                'text': 'Hello, World!'
+                'type': 'flex',
+                'contents': memeContent
             }]).then(
                 // document.getElementById('shareTargetPickerMessage').textContent = "Share target picker was launched."
             ).catch(function (res) {
