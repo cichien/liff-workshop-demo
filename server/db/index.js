1c925b4bc3794d24a286786e6589370e3317a1cc
Chien Cheng Yi <coke.chien@linecorp.com>
2020-10-14 08:01:32 +0800

    change the DATABASE env name

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.env.DATA_BASE_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: process.env.NODE_ENV !== 'development'
   }
