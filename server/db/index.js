2190a91346e801e3b5f033bcd8dcecbc1bb7f365
Chien Cheng Yi <coke.chien@linecorp.com>
2020-10-14 08:12:41 +0800

    set rejectUnauthorized to false

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: process.env.NODE_ENV !== 'development'
+    rejectUnauthorized: false
   }
 });
 module.exports = {
