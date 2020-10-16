78885325f44c6b14a62dd3ca7641f93a8bb4e362
Chien Cheng Yi <coke.chien@linecorp.com>
2020-10-17 00:05:57 +0800

    add server api campaign

--- server/routes/campaign.js
+++ server/routes/campaign.js
@@ -6,13 +6,36 @@ const router = express.Router();
 module.exports = router;
 
 router.post('/add', async (req, res) => {
-  res.send('add a campaign');
+  const { name, place, time } = req.body;
+  try {
+    const { rows } = await db.query('INSERT INTO campaign(name, place, time) VALUES($1, $2, $3) RETURNING *', [name, place, time]);
+    res.send(rows[0]);
+  } catch (error) {
+    console.error(error.stack);
+  }
 });
 
 router.get('/:id', async (req, res) => {
-  res.send('get a campaign');
+  const campaignId = req.params.id;
+  try {
+    const { rows } = await db.query('SELECT campaign.name as campaignName, member.name, member.is_join FROM campaign, member WHERE campaign_id = $1 AND campaign.id = $1', [campaignId]);
+    console.log(rows);
+    res.send(rows);
+  } catch (error) {
+    console.error(error.stack);
+  }
 });
 
 router.post('/:id/join', async (req, res) => {
-  res.send('join a campaign');
+  const { uid, name, isJoin } = req.body;
+  const campaignId = req.params.id;
+  try {
+    const { rows } = await db.query('SELECT * FROM member WHERE uid = $1 AND campaign_id = $2', [uid, campaignId]);
+    const { rows: result } = rows.length > 0
+      ? await db.query('UPDATE member SET is_join=$1 WHERE uid = $2 AND campaign_id = $3 RETURNING *', [isJoin, uid, campaignId])
+      : await db.query('INSERT INTO member(uid, name, is_join, campaign_id) VALUES($1, $2, $3, $4) RETURNING *', [uid, name, isJoin, campaignId]);
+    res.send(result[0]);
+  } catch (error) {
+    console.error(error.stack);
+  }
 });
