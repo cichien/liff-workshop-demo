1f2448160dbe2117d5ae153e1bb18975e4a43805
Chien Cheng Yi <coke.chien@linecorp.com>
2020-10-17 00:33:42 +0800

    do not use ssl in development

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,9 @@
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
+  ssl : process.env.NODE_ENV !== 'development' ? { 
     rejectUnauthorized: false
-  }
+  } : null
 });
 module.exports = {
   async query(text, params) {
